b2b4654f1d29da545cf68727b71a07b0c9ae600d
Nick Weihs <nick.weihs@gmail.com>
2017-01-27 05:48:28 -0800

    Fix parsing code

--- StormReflJson.h
+++ StormReflJson.h
@@ -228,6 +228,7 @@ inline bool StormReflJsonParseOverString(const char * str, const char *& result)
       case 'n':
       case 'r':
       case 't':
+        str++;
         break;
       case 'u':
         {
